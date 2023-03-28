dcb477a2d9ac77e59285c7d6dc7bbb5b16568cfd
JinKeeper <38350259+DeneryXd@users.noreply.github.com>
2023-03-28 17:46:28 +0300

    Add ver 1.0.1
    First version with 2 functions: StartGame and Start/Stop Guardian service on Oculus Quest 2

---
+++ Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace GameManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.startButton = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Off = new System.Windows.Forms.Button();
+            this.On = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("Century", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.startButton.Location = new System.Drawing.Point(271, 18);
+            this.startButton.Name = "startButton";
+            this.startButton.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.startButton.Size = new System.Drawing.Size(58, 37);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "START";
+            this.startButton.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.comboBox1.Font = new System.Drawing.Font("Tahoma", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(17, 27);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(112, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.Text = "Select Oculus";
+            this.comboBox1.UseWaitCursor = true;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.comboBox2.Font = new System.Drawing.Font("Tahoma", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(135, 27);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 4;
+            this.comboBox2.Text = "Select Game";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(275, 272);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Made by @JinKeeper";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(135, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(106, 55);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(2, 272);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "ver. 1.01";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(92, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Game Launcher";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.comboBox2);
+            this.panel1.Controls.Add(this.startButton);
+            this.panel1.Location = new System.Drawing.Point(15, 79);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(349, 71);
+            this.panel1.TabIndex = 9;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel2.Controls.Add(this.comboBox3);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.Off);
+            this.panel2.Controls.Add(this.On);
+            this.panel2.Location = new System.Drawing.Point(15, 170);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(349, 70);
+            this.panel2.TabIndex = 10;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.comboBox3.Font = new System.Drawing.Font("Tahoma", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(17, 27);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(112, 21);
+            this.comboBox3.TabIndex = 3;
+            this.comboBox3.Text = "Select Oculus";
+            this.comboBox3.UseWaitCursor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Location = new System.Drawing.Point(84, 4);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Guardian Controller";
+            // 
+            // Off
+            // 
+            this.Off.Font = new System.Drawing.Font("Century", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Off.Location = new System.Drawing.Point(271, 18);
+            this.Off.Name = "Off";
+            this.Off.Size = new System.Drawing.Size(58, 37);
+            this.Off.TabIndex = 0;
+            this.Off.Text = "OFF";
+            this.Off.UseVisualStyleBackColor = true;
+            this.Off.Click += new System.EventHandler(this.Off_Click_1);
+            // 
+            // On
+            // 
+            this.On.Font = new System.Drawing.Font("Century", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.On.Location = new System.Drawing.Point(207, 18);
+            this.On.Name = "On";
+            this.On.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.On.Size = new System.Drawing.Size(58, 37);
+            this.On.TabIndex = 0;
+            this.On.Text = "ON";
+            this.On.UseVisualStyleBackColor = true;
+            this.On.Click += new System.EventHandler(this.On_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(386, 289);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "AnotherWorldGameManager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Off;
+        private System.Windows.Forms.Button On;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
+namespace GameManager
+{
+    public partial class Form1 : Form
+    {
+        string AW1 = "192.168.50.225:5555";
+        string AW2 = "192.168.50.172:5555";
+        string AW3 = "192.168.50.205:5555";
+        string AW4 = "192.168.50.226:5555";
+        string AW5 = "192.168.50.253:5555";
+        string AW6 = "192.168.50.167:5555";
+        string AW7 = "192.168.50.251:5555";
+        string AW8 = "192.168.50.202:5555";
+        string Game1 = "com.owlchemylabs.jobsimulator/com.unity3d.player.UnityPlayerActivity";
+        string Game2 = "com.HomeNetGames.WW1oculus/com.unity3d.player.UnityPlayerActivity";
+        string Game3 = "com.resolutiongames.abvriop.santacruz/com.unity3d.player.UnityPlayerActivity";
+        string Game4 = "unity.SUPERHOT_Team.SUPERHOT_VR_QA/com.unity3d.player.UnityPlayerActivity";
+        string Game5 = "com.freelives.gorn/com.unity3d.player.UnityPlayerActivity";
+        string Game6 = "com.halfbrick.fruitninjavr/com.unity3d.player.UnityPlayerActivity";
+        string Game7 = "com.survios.Creed/com.epicgames.ue4.GameActivity";
+        string Game8 = "com.WenklyStudio.ElvenAssassin/com.unity3d.player.UnityPlayerActivity";
+        string Game9 = "com.beatgames.beatsaber/com.unity3d.player.UnityPlayerActivity";
+        string Game10 = "com.cloudheadgames.pistolwhip/com.unity3d.player.UnityPlayerActivity";
+        string Game11;
+        string selectedGame;
+        string selectedOculus;
+        string selectedOculusGuardian;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("AW1");
+            comboBox1.Items.Add("AW2");
+            comboBox1.Items.Add("AW3");
+            comboBox1.Items.Add("AW4");
+            comboBox1.Items.Add("AW5");
+            comboBox1.Items.Add("AW6");
+            comboBox1.Items.Add("AW7");
+            comboBox1.Items.Add("AW8");
+            comboBox3.Items.Add("AW1");
+            comboBox3.Items.Add("AW2");
+            comboBox3.Items.Add("AW3");
+            comboBox3.Items.Add("AW4");
+            comboBox3.Items.Add("AW5");
+            comboBox3.Items.Add("AW6");
+            comboBox3.Items.Add("AW7");
+            comboBox3.Items.Add("AW8");
+            comboBox2.Items.Add("JobSimulator");
+            comboBox2.Items.Add("WarPlanes");
+            comboBox2.Items.Add("AngryBirds");
+            comboBox2.Items.Add("SUPERHOT");
+            comboBox2.Items.Add("GORN");
+            comboBox2.Items.Add("Creed");
+            comboBox2.Items.Add("FruitNinja");
+            comboBox2.Items.Add("ElvenAssassin");
+            comboBox2.Items.Add("BeatSaber");
+            comboBox2.Items.Add("PistolWhip");
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            comboBox2.SelectedIndexChanged += comboBox2_SelectedIndexChanged;
+            comboBox3.SelectedIndexChanged += comboBox3_SelectedIndexChanged;
+            startButton.Click += new System.EventHandler(this.startButton_Click);
+            On.Click += new System.EventHandler(this.On_Click);
+            Off.Click += new System.EventHandler(this.Off_Click_1);
+
+
+
+        }
+        void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedOculus = comboBox1.SelectedItem.ToString();
+            
+        }
+        void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedOculusGuardian = comboBox3.SelectedItem.ToString();
+
+        }
+        void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedGame = comboBox2.SelectedItem.ToString();
+
+            if(selectedGame == "JobSimulator")
+            {
+                selectedGame = Game1;
+            }
+            else if (selectedGame == "WarPlanes")
+            {
+                selectedGame = Game2;
+            }
+            else if (selectedGame == "AngryBirds")
+            {
+                selectedGame = Game3;
+            }
+            else if (selectedGame == "SUPERHOT")
+            {
+                selectedGame = Game4;
+            }
+            else if (selectedGame == "GORN")
+            {
+                selectedGame = Game5;
+            }
+            else if (selectedGame == "Creed")
+            {
+                selectedGame = Game7;
+            }
+            else if (selectedGame == "FruitNinja")
+            {
+                selectedGame = Game6;
+            }
+            else if (selectedGame == "ElvenAssassin")
+            {
+                selectedGame = Game8;
+            }
+            else if (selectedGame == "BeatSaber")
+            {
+                selectedGame = Game9;
+            }
+            else if (selectedGame == "PistolWhip")
+            {
+                selectedGame = Game10;
+            }
+
+
+        }
+        public void startButton_Click(object sender, System.EventArgs e)
+        {
+            if (selectedOculus == "AW1")
+            {
+                selectedOculus = AW1;
+            }
+            else if (selectedOculus == "AW2")
+            {
+                selectedOculus = AW2;
+            }
+            else if (selectedOculus == "AW3")
+            {
+                selectedOculus = AW3;
+            }
+            else if (selectedOculus == "AW4")
+            {
+                selectedOculus = AW4;
+            }
+            else if (selectedOculus == "AW5")
+            {
+                selectedOculus = AW5;
+            }
+            else if (selectedOculus == "AW6")
+            {
+                selectedOculus = AW6;
+            }
+            else if (selectedOculus == "AW7")
+            {
+                selectedOculus = AW7;
+            }
+            else if (selectedOculus == "AW8")
+            {
+                selectedOculus = AW8;
+            }
+
+            ProcessStartInfo cmdStartGame = new ProcessStartInfo();
+            cmdStartGame.FileName = @"cmd.exe";
+            cmdStartGame.Arguments = $@"/c cd C:\\Another_World\\VR\\AndroidSDK_30.0.5\\platform-tools && adb -s {selectedOculusGuardian} shell am start -S -n {selectedGame}";
+            Process.Start(cmdStartGame);
+            MessageBox.Show("Игра запущена!");
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void On_Click(object sender, EventArgs e)
+        {
+            if (selectedOculusGuardian == "AW1")
+            {
+                selectedOculusGuardian = AW1;
+            }
+            else if (selectedOculusGuardian == "AW2")
+            {
+                selectedOculusGuardian = AW2;
+            }
+            else if (selectedOculusGuardian == "AW3")
+            {
+                selectedOculusGuardian = AW3;
+            }
+            else if (selectedOculusGuardian == "AW4")
+            {
+                selectedOculusGuardian = AW4;
+            }
+            else if (selectedOculusGuardian == "AW5")
+            {
+                selectedOculusGuardian = AW5;
+            }
+            else if (selectedOculusGuardian == "AW6")
+            {
+                selectedOculusGuardian = AW6;
+            }
+            else if (selectedOculusGuardian == "AW7")
+            {
+                selectedOculusGuardian = AW7;
+            }
+            else if (selectedOculusGuardian == "AW8")
+            {
+                selectedOculusGuardian = AW8;
+            }
+            ProcessStartInfo cmdEnableGuardian = new ProcessStartInfo();
+            cmdEnableGuardian.FileName = @"cmd.exe";
+            cmdEnableGuardian.Arguments = $@"/c cd C:\\Another_World\\VR\\AndroidSDK_30.0.5\\platform-tools && adb -s {selectedOculusGuardian} shell setprop debug.oculus.guardian_pause 0";
+            Process.Start(cmdEnableGuardian);
+            
+        }
+
+        private void Off_Click_1(object sender, EventArgs e)
+        {
+            if (selectedOculusGuardian == "AW1")
+            {
+                selectedOculusGuardian = AW1;
+            }
+            else if (selectedOculusGuardian == "AW2")
+            {
+                selectedOculusGuardian = AW2;
+            }
+            else if (selectedOculusGuardian == "AW3")
+            {
+                selectedOculusGuardian = AW3;
+            }
+            else if (selectedOculusGuardian == "AW4")
+            {
+                selectedOculusGuardian = AW4;
+            }
+            else if (selectedOculusGuardian == "AW5")
+            {
+                selectedOculusGuardian = AW5;
+            }
+            else if (selectedOculusGuardian == "AW6")
+            {
+                selectedOculusGuardian = AW6;
+            }
+            else if (selectedOculusGuardian == "AW7")
+            {
+                selectedOculusGuardian = AW7;
+            }
+            else if (selectedOculusGuardian == "AW8")
+            {
+                selectedOculusGuardian = AW8;
+            }
+            ProcessStartInfo cmdDisableGuardian = new ProcessStartInfo();
+            cmdDisableGuardian.FileName = @"cmd.exe";
+            cmdDisableGuardian.Arguments = $@"/c cd C:\\Another_World\\VR\\AndroidSDK_30.0.5\\platform-tools && adb -s {selectedOculusGuardian} shell setprop debug.oculus.guardian_pause 1";
+            Process.Start(cmdDisableGuardian);
+            
+        }
+    }
+}
